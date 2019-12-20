1c8e0cacba78b4693b8569e2df419dc2d8c8d77c
dzub1996 <55141107+dzub1996@users.noreply.github.com>
2019-12-20 16:09:33 +0300

    json

--- addons/educenter/controllers/controllers.py
+++ addons/educenter/controllers/controllers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+import json
 
 # class Educenter(http.Controller):
 #     @http.route('/educenter/educenter/', auth='public')
@@ -18,3 +19,16 @@ from odoo import http
 #         return http.request.render('educenter.object', {
 #             'object': obj
 #         })
+class InfoTimetable(http.Controller):
+    @http.route('/ajax/search_schedule', auth='public', type='json', csrf=False, methods=['POST', 'OPTIONS'], cors='*')
+    def AjaxSearchSchedule(self, query=''):
+        if http.request.httprequest.method == 'OPTIONS':
+            return json.dump([''])
+# @http.route('/ajax/search_schedule', auth='public', type='json', methods=['POST'])
+#     def AjaxSearchSchedule(self, query=''):
+        objects = http.request.env['educenter.courses'].sudo().search([
+            ('name', 'ilike', query)
+        ], limit=10, order="name asc")
+        return json.dumps(
+            [{'id': obj.id, 'price': obj.name} for obj in objects]
+        )
